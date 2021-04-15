aeef54c6aad761d8d60305ab6bf5952863a39239
Greg Moua <74290864+gregmoua19@users.noreply.github.com>
2021-04-15 00:03:46 -0700

    Add files via upload
    Updated mongo.js to run properly with new ceremony.js and names.js endpoints.

--- oscars_api/lib/mongo.js
+++ oscars_api/lib/mongo.js
@@ -1,70 +1,164 @@
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/oscars";
-const dbName = "oscars";
+const dbName = "oscars"
 const client = new MongoClient(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-client.connect(function (error) {
-  if (error) throw error;
-  console.log("Connection to mongo successfull...");
-});
+client.connect(function(error){
+  if(error) throw error;
+  console.log("Connection to mongo successful...")
+})
 
+//grabs all categories
 const getCategories = async () => {
   return new Promise((resolve, reject) => {
-    const db = client.db(dbName);
+    const db = client.db(dbName)
 
     resolve(db.collection("data").distinct("category"));
-  });
-};
 
-const getCategory = async (category) => {
+  });
+}
+
+//grabs specific category
+const getCategory = async(category) => {
   return new Promise((resolve, reject) => {
-    const db = client.db(dbName);
+    const db = client.db(dbName)
 
-    const categoryQuery = { category: category };
+    const categoryQuery = {category: category};
+    
+    db.collection("data").find(categoryQuery).toArray(function(error, result){
+      if(error) reject(error);
+      resolve(result);
+    });
 
-    db.collection("data")
-      .find(categoryQuery)
-      .toArray(function (error, result) {
-        if (error) reject(error);
-        resolve(result);
-      });
   });
-};
+}
 
-const getFilmYear = async (year) => {
+//gets all movies made during a specific year
+const getFilmYear = async(year) => {
   return new Promise((resolve, reject) => {
-    const db = client.db(dbName);
+    const db = client.db(dbName)
 
-    const filmYearQuery = { year_film: year };
+    const filmYearQuery = {year_film: year};
 
-    db.collection("data")
-      .find(filmYearQuery)
-      .toArray(function (error, result) {
-        if (error) reject(error);
-        resolve(result);
-      });
+    db.collection("data").find(filmYearQuery).toArray(function(error, result){
+      if(error) reject(error);
+      resolve(result);
+    });
+
   });
-};
+}
 
-const getCeremonyYear = async (year) => {
+//gets all movies made during specific ceremony year
+const getCeremonyYear = async(year) => {
   return new Promise((resolve, reject) => {
-    const db = client.db(dbName);
+    const db = client.db(dbName)
 
-    const ceremonyYearQuery = { year_ceremony: year };
+    const ceremonyYearQuery = {year_ceremony: year};
 
-    db.collection("data")
-      .find(ceremonyYearQuery)
-      .toArray(function (error, result) {
-        if (error) reject(error);
-        resolve(result);
-      });
+    db.collection("data").find(ceremonyYearQuery).toArray(function(error, result){
+      if(error) reject(error);
+      resolve(result);
+    });
+
   });
-};
+}
 
+//gets all movies made during specific ceremony
+const getCeremonyNumber = async(ceremonyNumber) => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName)
+
+    const ceremonyNumberQuery = {ceremony: ceremonyNumber};
+
+    db.collection("data").find(ceremonyNumberQuery).toArray(function(error, result){
+      if(error) reject(error);
+      resolve(result);
+    });
+  });
+}
+
+//working on
+//gets all winners 
+const getResults = async(winOrLose) => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName)
+
+    const resultQuery = db.collection("data").find({ winner:'True'});
+
+    /*db.collection("data").find(resultQuery).toArray(function(error, result){
+      if(error) reject(error);
+      resolve(result);
+    });
+*/
+  });
+}
+
+//gets specific movie results
+const getFilmName = async(filmName) => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName)
+
+    const filmQuery= {film: filmName};
+
+    db.collection("data").find(filmQuery).toArray(function(error, result){
+      if(error) reject(error);
+      resolve(result);
+    });
+
+  });
+}
+
+//stillworking on
+//lists all the movies
+const getFilmNames = async () => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName)
+
+    //const filmNamesQuery = {film: {} };
+    resolve(db.collection("data").find("film"));
+
+    // db.collection("data").find({}).toArray(function(error, result){
+    //  if(error) reject(error);
+    //   resolve(result);
+    //});
+
+  });
+}
+
+//still working on
+//lists all involvement of a specific actor/actress
+const getActorName = async(actorName) => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName)
+
+    const actorQuery= {name: actorName};
+
+    db.collection.find({name: actorName});
+
+  });
+}
+
+//not done yet
+//method will get years between and pull all movies in between date
+//commenting out so it doesnt mess up code
+/*const greaterLessThan = async(year1, year2) => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName)
+
+    db.
+  })
+}*/
+
+
+module.exports.getFilmNames = getFilmNames;
+module.exports.getActorName = getActorName;
+module.exports.getFilmName = getFilmName;
+module.exports.getResults = getResults;
 module.exports.getCategories = getCategories;
 module.exports.getCategory = getCategory;
 module.exports.getFilmYear = getFilmYear;
 module.exports.getCeremonyYear = getCeremonyYear;
+module.exports.getCeremonyNumber = getCeremonyNumber;
